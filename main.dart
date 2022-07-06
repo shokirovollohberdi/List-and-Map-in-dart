0bd967dee56478962a7dce7f3a51a2c6edf7f297
Ollohberdi <85603736+shokirovollohberdi@users.noreply.github.com>
2022-07-06 10:41:05 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,27 @@
+void main() {
+  /*
+           List      
+	List a = [12,32,43,45,65];
+  List b = [32,43];
+  a.add(90);
+   print(a);
+   a.clear(); // clear the a list
+  print(a.isEmpty);
+   a.addAll(b); // add b list to a list
+  print(a);
+  print(a.isEmpty);
+  print(a.contains(80)); // does 80 have in a list
+  */
+
+  /*        Map
+  Map a = { "a":13,"b":15, "c":12,5:5 };
+  
+  print(a);
+  Map b= {0:45, 7:7,"a":6,"n":null};
+  a.clear();
+  a.addAll(b);
+  print(a);
+
+  print(a["n"]);
+  */
+}
